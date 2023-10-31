27c97b3b1e3d08cad4446277924b9512c8e76c7f
tod619 <trevorodwyer619@hotmail.com>
2023-10-31 21:19:21 +0000

    Delete obsolete code in views.py

--- challenges/views.py
+++ challenges/views.py
@@ -3,15 +3,6 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eat no meat for the whole month")
-
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes per day")
-
-def march(request):
-    return HttpResponse("Learn Django for 20 minutes every day")
-
 def monthly_challenges(request, month):
     challenge_text = None
     
