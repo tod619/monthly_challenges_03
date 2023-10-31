dee6a1c36c4940a0aa2996215e8fbabf06277fd5
tod619 <trevorodwyer619@hotmail.com>
2023-10-31 20:59:19 +0000

    Add second url

--- challenges/views.py
+++ challenges/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello World")
+def january(request):
+    return HttpResponse("Eat no meat for the whole month")
+
+def february(request):
+    return HttpResponse("Walk for at least 20 minutes per day")
 
