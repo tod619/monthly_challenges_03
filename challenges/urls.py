b1337905021824bf4b95c0ced4e3e9866479bafa
tod619 <trevorodwyer619@hotmail.com>
2023-11-01 21:11:32 +0000

    Create index function in views.py file

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path("", views.index),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -19,6 +19,18 @@ monthly_challenges = {
 
 
 # Create your views here.
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitilized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href = '{month_path}'>{capitilized_month}</a></li>"
+        response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request,month):
     months = list(monthly_challenges.keys())
 
