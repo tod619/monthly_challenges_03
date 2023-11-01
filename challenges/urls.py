6db0b2012db9b44abc69ffbd2d2759d01d560894
tod619 <trevorodwyer619@hotmail.com>
2023-11-01 20:03:45 +0000

    Add path convertor to challenges

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    
-    path("<month>", views.monthly_challenges)
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenges)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+def monthly_challenge_by_number(request,month):
+    return HttpResponse(month)
+
 
 def monthly_challenges(request, month):
     challenge_text = None
