2fdd9e0e899067a8bfa1a75920fbe0a991404a62
tod619 <trevorodwyer619@hotmail.com>
2023-11-01 20:31:30 +0000

    Create redirect for challenges

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 monthly_challenges = {
     "january": "Don't eat meat for the whole month",
@@ -19,7 +19,14 @@ monthly_challenges = {
 
 # Create your views here.
 def monthly_challenge_by_number(request,month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Month not supported")
+
+    redirect_month = months[month - 1]
+
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 
 def monthly_challenge(request, month):
