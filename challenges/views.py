c526cd644b27570a3d1e8b11ca04f769f19fb0ab
tod619 <trevorodwyer619@hotmail.com>
2023-11-01 20:45:51 +0000

    Create redirect using dynamic reverse function

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Don't eat meat for the whole month",
@@ -25,8 +26,9 @@ def monthly_challenge_by_number(request,month):
         return HttpResponseNotFound("Month not supported")
 
     redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
 
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
